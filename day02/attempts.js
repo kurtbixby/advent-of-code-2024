8eb73cc6992eadefb73738a669b8d5e001001b64
Kurt Bixby <kurtbixby@gmail.com>
2024-12-03 16:30:21 -0600

    Finished part 2 naively

--- day02/attempts.js
+++ day02/attempts.js
@@ -1,45 +1,3 @@
-const assert = require("node:assert");
-const readline = require("node:readline");
-
-function setup() {
-    let rl = readline.createInterface(process.stdin);
-    let reports = [];
-    rl.on("line", (input) => {
-        const report = input.split(/\s+/);
-        reports.push(report);
-    });
-
-    rl.once("close", () => main({reports: reports}));
-}
-
-function main({reports}) {
-    const safeReports = reports.map(r => calculateReportSafety(r)).reduce((acc, isSafe) => acc + (isSafe ? 1 : 0), 0);
-    console.log(`# of safe reports: ${safeReports}`);
-
-    const safeReportsDampener = reports.map(r => calculateReportSafetyDampenedNaive(r)).reduce((acc, isSafe) => acc + (isSafe ? 1 : 0), 0);
-    console.log(`# of safe r eports with dampener: ${safeReportsDampener}`);
-}
-
-function calculateReportSafety(report) {
-    let isSafe = true;
-
-    assert(report.length >= 2);
-    let monotonicSign = sign(report[1] - report[0]);
-
-    for (let i = 0; i < report.length - 1; i++) {
-        const cur = report[i];
-        const next = report[i+1];
-
-        const diff = Math.abs(cur - next);
-        if (diff < 1 || diff > 3 || sign(next - cur) !== monotonicSign) {
-            isSafe = false;
-            break;
-        }
-    }
-
-    return isSafe;
-}
-
 function calculateReportSafetyWithDampener(report) {
     let isSafe = true;
     assert(report.length >= 3);
@@ -166,17 +124,6 @@ function calculateReportSafetyWithDampener3(report) {
     return true;
 }
 
-function calculateReportSafetyDampenedNaive(report) {
-    for (let i = 0; i < report.length; i++) {
-        const safe = calculateReportSafety(report.toSpliced(i, 1));
-        if (safe) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 function calculateReportSafetyWithDampenerAlt(report) {
     let isSafe = true;
     assert(report.length >= 3);
@@ -211,13 +158,3 @@ function calculateReportSafetyWithDampenerAlt(report) {
 
     return isSafe;
 }
-
-function sign(number) {
-    return number < 0 ? -1 : 1;
-}
-
-function clamp(number, min, max) {
-    return Math.min(max, Math.max(min, number));
-}
-
-setup();
