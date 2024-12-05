f1394d7bbc6302ddfa03d8ce64950021af8680e7
Kurt Bixby <kurtbixby@gmail.com>
2024-12-05 16:59:45 -0600

    Day 4 Part 2 Rust

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let grid = read_input();
     // println!("{:?}", grid);
     println!("{:?}", count_xmas(grid.as_slice()));
+    println!("{:?}", count_cross_mas(grid.as_slice()));
     
 }
 
@@ -42,7 +43,10 @@ fn check_xmas_in_dir(letter_grid: &[Vec<char>], location: &(u32, u32), dir: (i32
     let search = ['X', 'M', 'A', 'S'];
 
     for i in 0i32..search.len() as i32 {
-        if *row as i32 + (i * v_dir) >= letter_grid.len().try_into().unwrap() || *row as i32 + (i * v_dir) < 0 || *column as i32 + (i * h_dir) >= letter_grid[0].len().try_into().unwrap() || *column as i32 + (i * h_dir) < 0 {
+        if *row as i32 + (i * v_dir) >= letter_grid.len().try_into().unwrap()
+        || *row as i32 + (i * v_dir) < 0
+        || *column as i32 + (i * h_dir) >= letter_grid[0].len().try_into().unwrap()
+        || *column as i32 + (i * h_dir) < 0 {
             return 0;
         }
         if letter_grid[(*row as i32 + (i*v_dir)) as usize][(*column as i32 + (i*h_dir)) as usize] != search[i as usize] {
@@ -51,6 +55,43 @@ fn check_xmas_in_dir(letter_grid: &[Vec<char>], location: &(u32, u32), dir: (i32
     }
     return 1;
 }
+
+fn count_cross_mas(letter_grid: &[Vec<char>]) -> u32 {
+    let mut count = 0;
+    for i in 0..letter_grid.len() {
+        for j in 0..letter_grid[0].len() {
+            if letter_grid[i][j] == 'A'
+            && check_cross_mas(letter_grid, (i.try_into().unwrap(), j.try_into().unwrap())) {
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+fn check_cross_mas(letter_grid: &[Vec<char>], location: (u32, u32)) -> bool {
+    let (row, column) = location;
+    if (row as usize) + 1 >= letter_grid.len()
+    || (row as usize) < 1
+    || (column as usize) + 1 >= letter_grid[0].len()
+    || (column as usize) < 1 {
+        return false;
+    }
+
+    //check TLBR
+    let tl = letter_grid[row as usize -1][column as usize -1];
+    let br = letter_grid[row as usize +1][column as usize +1];
+    let tlbr = (tl == 'M' && br == 'S') || (tl == 'S' && br == 'M');
+
+    //check TRBL
+    let tr = letter_grid[row as usize -1][column as usize +1];
+    let bl = letter_grid[row as usize +1][column as usize -1];
+    let trbl = (tr == 'M' && bl == 'S') || (tr == 'S' && bl == 'M');
+
+    return tlbr && trbl;
+}
+
 
 // if i == 0 && j == 0 {
 //     continue;
