ac6be8ba2e4777546726d1dfa273f8eea8f34fe1
Kurt Bixby <kurtbixby@gmail.com>
2024-12-05 22:11:29 -0600

    Day 5 Part 1 Rust

---
+++ day05/src/main.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+fn main() {
+    let (rules, updates) = read_input();
+    println!("{:?}", part1(updates.as_slice(), &rules));
+}
+
+fn read_input() -> (HashMap<u32,HashSet<u32>>, Vec<Vec<u32>>) {
+    let lines = std::io::stdin().lines();
+
+    let mut rules: HashMap<u32,HashSet<u32>> = HashMap::new();
+    let mut iter = lines.into_iter();
+    loop {
+        match iter.next() {
+            None => break,
+            Some(line) => {
+                let line_val = line.unwrap();
+                if line_val == "" {
+                    break;
+                }
+                
+                let mut split = line_val.split('|');
+                let rule = split.next().unwrap().parse::<u32>().unwrap();
+                let restriction = split.next().unwrap().parse::<u32>().unwrap();
+                if rules.contains_key(&rule) {
+                    rules.get_mut(&rule).unwrap().insert(restriction);
+                } else {
+                    let mut set = HashSet::new();
+                    set.insert(restriction);
+                    rules.insert(rule, set);
+                }
+            }
+        }
+    }
+
+    let updates = iter.map(|line| {line.unwrap().split(',').map(str::parse::<u32>).map(Result::unwrap).collect()}).collect();
+
+    return (rules, updates);
+}
+
+fn part1(updates: &[Vec<u32>], rules: &HashMap<u32,HashSet<u32>>) -> u32 {
+    updates.iter().filter(|update| is_print_safe(&update, &rules)).fold(0, |acc, update| acc + update.get(update.len() / 2).unwrap())
+}
+
+fn is_print_safe(update: &[u32], rules: &HashMap<u32,HashSet<u32>>) -> bool {
+    let mut printed_pages = HashSet::new();
+    for page in update {
+        let restrictions = rules.get(page).unwrap();
+        if restrictions.iter().map(|restriction| printed_pages.contains(restriction)).any(|val| val) {
+            return false;
+        }
+        printed_pages.insert(page);
+    }
+
+    return true;
+}
