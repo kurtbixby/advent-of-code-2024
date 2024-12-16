85ae9f27c38644ecddae06678906cfe541590e0a
Kurt Bixby <kurtbixby@gmail.com>
2024-12-15 23:36:26 -0600

    Day 7 Part 2 JS

--- day07/index.js
+++ day07/index.js
@@ -15,13 +15,18 @@ function setup() {
 }
 
 function main(calibrations) {
-    const result = calibrations.reduce((sum, cal) => sum + findPossibleCalibrations(cal), 0);
-    console.log(result);
+    const result1 = calibrations.reduce((sum, cal) => sum + findPossibleCalibrations(cal, ["+", "*"]), 0);
+    console.log("Part 1: " + result1);
+    const result2 = calibrations.reduce((sum, cal) => sum + findPossibleCalibrations(cal, ["+", "*", "||"]), 0);
+    console.log("Part 2: " + result2);
 }
 
-function findPossibleCalibrations(calibration) {
+function findPossibleCalibrations(calibration, ops) {
     // console.log(calibration);
-    const count = solveRecursive(calibration.total, calibration.nums[0], calibration.nums.slice(1), ["+", "*"]);
+    const count = solveRecursive(calibration.total, calibration.nums[0], calibration.nums.slice(1), ops);
+    // if (count > 0) {
+    //     console.log(calibration.total);
+    // }
     // console.log(count);
     return count > 0 ? calibration.total : 0;
 }
@@ -37,13 +42,20 @@ function solveRecursive(goal, sum, nums, ops) {
     let count = 0;
     const num = nums[0];
     for (const op of ops) {
-        let opSum = sum;
         if (op === "+") {
-            opSum += num;
+            count += solveRecursive(goal, sum + num, nums.slice(1), ops);
         } else if (op === "*") {
-            opSum *= num;
+            count += solveRecursive(goal, sum * num, nums.slice(1), ops);
+        } else if (op === "||") {
+            count += solveRecursive(goal, parseInt(String(sum).concat(num)), nums.slice(1), ops);
+            // const prefixMatch = String(goal).startsWith(sum);
+            // if (prefixMatch) {
+            //     const subGoal = String(goal).substring(String(sum).length);
+            //     console.log("Goal: " + goal);
+            //     console.log("Subgoal: " + subGoal);
+            //     count += solveRecursive(subGoal, num, nums.slice(1), ops)
+            // }
         }
-        count += solveRecursive(goal, opSum, nums.slice(1), ops);
     }
     return count;
 }
