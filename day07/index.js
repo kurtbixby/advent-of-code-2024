28156f6e68b2464a4a4b9147d41736272dd48b42
Kurt Bixby <kurtbixby@gmail.com>
2024-12-15 23:54:21 -0600

    Added iterative solution (slower)

--- day07/index.js
+++ day07/index.js
@@ -15,26 +15,23 @@ function setup() {
 }
 
 function main(calibrations) {
-    const result1 = calibrations.reduce((sum, cal) => sum + findPossibleCalibrations(cal, ["+", "*"]), 0);
+    const result1 = calibrations.reduce((sum, cal) => sum + findPossibleCalibrationsRecursive(cal, ["+", "*"]), 0);
     console.log("Part 1: " + result1);
-    const result2 = calibrations.reduce((sum, cal) => sum + findPossibleCalibrations(cal, ["+", "*", "||"]), 0);
+    const result2 = calibrations.reduce((sum, cal) => sum + findPossibleCalibrationsRecursive(cal, ["+", "*", "||"]), 0);
     console.log("Part 2: " + result2);
 }
 
-function findPossibleCalibrations(calibration, ops) {
-    // console.log(calibration);
+function findPossibleCalibrationsRecursive(calibration, ops) {
     const count = solveRecursive(calibration.total, calibration.nums[0], calibration.nums.slice(1), ops);
-    // if (count > 0) {
-    //     console.log(calibration.total);
-    // }
-    // console.log(count);
+    return count > 0 ? calibration.total : 0;
+}
+
+function findPossibleCalibrations(calibration, ops) {
+    const count = solveQueue(calibration.total, calibration.nums, ops);
     return count > 0 ? calibration.total : 0;
 }
 
 function solveRecursive(goal, sum, nums, ops) {
-    // console.log("~~ RECURSIVE ~~")
-    // console.log("Sum: " + sum);
-    // console.log("Nums: " + nums);
     if (nums.length === 0) {
         return goal === sum ? 1 : 0;
     }
@@ -48,15 +45,36 @@ function solveRecursive(goal, sum, nums, ops) {
             count += solveRecursive(goal, sum * num, nums.slice(1), ops);
         } else if (op === "||") {
             count += solveRecursive(goal, parseInt(String(sum).concat(num)), nums.slice(1), ops);
-            // const prefixMatch = String(goal).startsWith(sum);
-            // if (prefixMatch) {
-            //     const subGoal = String(goal).substring(String(sum).length);
-            //     console.log("Goal: " + goal);
-            //     console.log("Subgoal: " + subGoal);
-            //     count += solveRecursive(subGoal, num, nums.slice(1), ops)
-            // }
         }
     }
+    return count;
+}
+
+function solveQueue(goal, nums, ops) {
+    let q = [];
+    let count = 0;
+
+    q.push({sum: 0, nums: nums});
+    while (q.length !== 0) {
+        let {sum, nums} = q.pop();
+        if (nums.length === 0) {
+            count += (goal === sum ? 1 : 0);
+            continue;
+        }
+        const num = nums[0];
+        for (const op of ops) {
+            let opSum = 0;
+            if (op === "+") {
+                opSum = sum + num;
+            } else if (op === "*") {
+                opSum = sum * num;
+            } else if (op === "||") {
+                opSum = parseInt(String(sum).concat(num));
+            }
+            q.push({sum: opSum, nums: nums.slice(1)});
+        }
+    }
+
     return count;
 }
 
